503c6f5b0fd79f4d1f615027e91784dcec05783a
khafrewatkins <khafrewatkins@gmail.com>
2021-08-18 15:41:06 -0500

    'strategy'

--- services/passport.js
+++ services/passport.js
@@ -20,7 +20,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: 'http://localhost:3000/auth/google/callback'
+      callbackURL: 'http://localhost:3000/auth/google/callback',
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
